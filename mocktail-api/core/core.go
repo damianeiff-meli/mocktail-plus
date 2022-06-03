7c75141c6bbf3a339f5ccc566f68d375d99ccbb6
damianeiff-meli <damian.eiff@mercadolibre.com>
2022-06-03 09:29:50 -0300

    feat: add http methods allowed

--- mocktail-api/core/core.go
+++ mocktail-api/core/core.go
@@ -1,23 +1,35 @@
 package core
 
 import (
-	"mocktail-api/database"
-
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"gorm.io/datatypes"
+	"mocktail-api/database"
+	"net/http"
 )
 
 type Api struct {
-	ID uint `gorm:"primary_key;auto_increment;not_null"`
-	Endpoint string `validate:"required"`
-	Method string `validate:"is-method-allowed"`
-	Key string `gorm:"unique;not null"`
+	ID       uint           `gorm:"primary_key;auto_increment;not_null"`
+	Endpoint string         `validate:"required"`
+	Method   string         `validate:"is-method-allowed"`
+	Key      string         `gorm:"unique;not null"`
 	Response datatypes.JSON `validate:"required"`
 }
 type Apis struct {
 	Apis []Api `validate:"required"`
+}
+
+var httpMethodList = [...]string{
+	http.MethodGet,
+	http.MethodHead,
+	http.MethodPost,
+	http.MethodPut,
+	http.MethodPatch,
+	http.MethodDelete,
+	http.MethodConnect,
+	http.MethodOptions,
+	http.MethodTrace,
 }
 
 func GetApis(c *fiber.Ctx) error {
@@ -30,7 +42,7 @@ func GetApis(c *fiber.Ctx) error {
 func CreateApi(c *fiber.Ctx) error {
 	api := new(Api)
 	if err := c.BodyParser(api); err != nil {
-		return c.Status(503).JSON(fiber.Map{"message":err.Error()})
+		return c.Status(503).JSON(fiber.Map{"message": err.Error()})
 	}
 	if err := InsertApi(api); err != nil {
 		return c.Status(400).JSON(fiber.Map{"message": err.Error()})
@@ -38,9 +50,9 @@ func CreateApi(c *fiber.Ctx) error {
 	return c.JSON(api)
 }
 
-func InsertApi(api *Api) error{
+func InsertApi(api *Api) error {
 	db := database.DBConn
-	api.Key = api.Method+api.Endpoint
+	api.Key = api.Method + api.Endpoint
 	validate := validator.New()
 	validate.RegisterValidation("is-method-allowed", isApiHTTPMethodValid)
 	if err := validate.Struct(api); err != nil {
@@ -49,7 +61,7 @@ func InsertApi(api *Api) error{
 	if err := db.Create(&api).Error; err != nil {
 		return err
 	}
-	
+
 	return nil
 }
 
@@ -59,10 +71,10 @@ func DeleteApiByKey(c *fiber.Ctx) error {
 	var api Api
 
 	if err := db.Unscoped().Delete(&api, "ID = ? ", id).Error; err != nil {
-		return c.Status(400).JSON(fiber.Map{"message":err.Error()})
+		return c.Status(400).JSON(fiber.Map{"message": err.Error()})
 	}
-	
-	return c.Status(200).JSON(fiber.Map{"message":"Completed"})
+
+	return c.Status(200).JSON(fiber.Map{"message": "Completed"})
 }
 
 func ExportApis(c *fiber.Ctx) error {
@@ -76,22 +88,20 @@ func ImportApis(c *fiber.Ctx) error {
 	apis := new(Apis)
 
 	if err := c.BodyParser(apis); err != nil {
-		return c.Status(400).JSON(fiber.Map{"message":err.Error()})
+		return c.Status(400).JSON(fiber.Map{"message": err.Error()})
 	}
 
 	for i := 0; i < len(apis.Apis); i++ {
 		InsertApi(&apis.Apis[i])
-    }
-	return c.Status(200).JSON(fiber.Map{"message":`Completed.`})
+	}
+	return c.Status(200).JSON(fiber.Map{"message": `Completed.`})
 }
 
-
 func isApiHTTPMethodValid(fl validator.FieldLevel) bool {
-	HTTPMethodList := [5]string{"GET", "POST", "PUT", "PATCH", "DELETE"}
-    for _, b := range HTTPMethodList {
-        if b == fl.Field().String() {
-            return true
-        }
-    }
-    return false	
+	for _, b := range httpMethodList {
+		if b == fl.Field().String() {
+			return true
+		}
+	}
+	return false
 }
